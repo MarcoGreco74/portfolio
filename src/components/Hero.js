53b8c9a35d00a3a007510f07df2f638bf68ff756
MarcoGreco74 <marcogreco.gibson@gmail.com>
2025-07-31 19:12:34 +0200

    fix: add new skills

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section className="hero">
       <h2>{"\u{1F44B}"}Ciao, sono Marco</h2>
-      <p>Sviluppatore Web | Java · React · JavaScript · PHP · Node.js · MySQL</p>
+      <p>Sviluppatore Web | Java · Spring Boot · React · JavaScript · PHP · Node.js · MySQL</p>
     </section>
   );
 };
